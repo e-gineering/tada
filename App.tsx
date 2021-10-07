66fbeee2d8740b220ab73d68c54654311db01378
Robbie Page <robbie.page@e-gineering.com>
2021-10-06 20:45:49 -0400

    update icon and status bar

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Tada!</Text>
-      <StatusBar style="auto" />
+      <StatusBar backgroundColor="#448aff" barStyle="light-content" />
     </View>
   );
 }
